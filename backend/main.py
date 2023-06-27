5b2a9c2ae96836b55af4887262fc3ae913a90656
SIYEA J <siyea0405@gmail.com>
2023-06-27 23:14:50 +0900

    feat: Create FastAPI main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from backend.api.endpoints import products_router
 
 app = FastAPI()
+
+app.include_router(products_router, prefix="/api/endpoints")
+
 
 @app.get("/")
 def read_root():
