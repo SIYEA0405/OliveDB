511b5ebf83fd43196989247762bb57527422df39
SIYEA J <siyea0405@gmail.com>
2023-07-12 21:57:53 +0900

    reactor: Add SearchBar in index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { Grid, Box } from "@chakra-ui/react";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
@@ -13,13 +13,10 @@ export default function Home() {
           content="OliveDB is database of everything on Oliveyoung."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/oliveDB_logo.ico" />
       </Head>
       <main className={styles.main}>
-        <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} gap={6}>
-          <Box>Column 1</Box>
-          <Box>Column 2</Box>
-        </Grid>
+        <SearchBar />
       </main>
     </>
   );
