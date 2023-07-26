b5cb3c6dccee4bd13e6104ac47a6b016fe8202d6
SIYEA J <siyea0405@gmail.com>
2023-07-26 23:57:48 +0900

    fix: Temporary resolution of errors

--- frontend/src/pages/showProduct.tsx
+++ frontend/src/pages/showProduct.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import ProductTable from "@/components/ProductTable";
 import ProductChart from "@/components/ProductChart";
@@ -17,7 +16,7 @@ export default function showProduct() {
       </Head>
       <main className={styles.main}>
         <h1>결과창</h1>
-        <ProductTable />
+        {/* <ProductTable /> */}
         <ProductChart
           data={[1, 2, 3, 4, 5]}
           labels={["One", "Two", "Three", "Four", "Five"]}
