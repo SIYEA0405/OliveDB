be50ce806728e4618fb0aab07fcabc1ca0db0d55
SIYEA J <siyea0405@gmail.com>
2023-07-28 19:59:45 +0900

    refactor: Add Head tag to Layout component and change Footer position

--- frontend/src/pages/showProduct.tsx
+++ frontend/src/pages/showProduct.tsx
@@ -5,15 +5,6 @@ import ProductChart from "@/components/ProductChart";
 export default function showProduct() {
   return (
     <>
-      <Head>
-        <title>OliveDB</title>
-        <meta
-          name="description"
-          content="OliveDB is database of everything on Oliveyoung."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/oliveDB_logo.ico" />
-      </Head>
       <main className={styles.main}>
         <h1>결과창</h1>
         {/* <ProductTable /> */}
--- frontend/src/pages/showProductList.tsx
+++ frontend/src/pages/showProductList.tsx
@@ -17,21 +17,11 @@ interface ProductDataProps {
 
 interface ShowProductListProps {
   products: ProductDataProps[];
-
 }
 
 const ShowProductList: React.FC<ShowProductListProps> = ({ products }) => {
   return (
     <>
-      <Head>
-        <title>OliveDB</title>
-        <meta
-          name="description"
-          content="OliveDB is database of everything on Oliveyoung."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/oliveDB_logo.ico" />
-      </Head>
       <main className={styles.main}>
         <h1>{}로 검색한 결과는 총 0개 입니다.</h1>
       </main>
