7326034074a4c42e14dd9b8920ed1809213245b7
SIYEA J <siyea0405@gmail.com>
2023-08-24 17:57:25 +0900

    feat: Apply react-query and react-query devtools

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 import Layout from "../components/layout/Layout";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ChakraProvider>
   );
