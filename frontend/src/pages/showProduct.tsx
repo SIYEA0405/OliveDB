82c5485b765fefb228062581b8fef8c9a6e44624
SIYEA J <siyea0405@gmail.com>
2023-08-25 21:05:24 +0900

    fix: Disabled Chart.js

--- frontend/src/pages/showProduct.tsx
+++ frontend/src/pages/showProduct.tsx
@@ -1,17 +1,12 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import ProductTable from "@/components/ProductTable";
-import ProductChart from "@/components/ProductChart";
 export default function showProduct() {
   return (
     <>
       <main className={styles.main}>
         <h1>결과창</h1>
         {/* <ProductTable /> */}
-        <ProductChart
-          data={[1, 2, 3, 4, 5]}
-          labels={["One", "Two", "Three", "Four", "Five"]}
-        />
       </main>
     </>
   );
