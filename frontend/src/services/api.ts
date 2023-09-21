79fe0cfa922b6f935b6c2588a160cb1618068254
SIYEA J <siyea0405@gmail.com>
2023-09-21 20:07:39 +0900

    feat: Create default setting showProduct.tsx page

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,3 +9,13 @@ export const fetchProducts = async (searchTerm: SearchTermProps) => {
   });
   return response.data;
 };
+
+export const fetchPriceByDate = async (searchTerm: SearchTermProps) => {
+  const response = await axios.get("/api/getPriceByDate", {
+    params: {
+      search_id: searchTerm,
+    },
+  });
+
+  return response.data;
+};
